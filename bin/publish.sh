935492c6f0227b206c989b904b16e948a3e96119
Williams <TCO04004@leeds.gov.uk>
2016-12-15 12:11:11 +0000

    publish

--- bin/publish.sh
+++ bin/publish.sh
@@ -4,15 +4,12 @@ VERSION_LATEST=`cat package.json | jq -r '.version'`
 VERSION_REGISTRY=`npm view generator-lcc-sharepoint version`
 echo "VERSION_LATEST $VERSION_LATEST";
 echo "VERSION_REGISTRY $VERSION_REGISTRY";
-#V_LATEST=$VERSION_LATEST | tr -d "\"\`'"
-#echo "V_LATEST $V_LATEST";
-
 
 if [ "$VERSION_LATEST" != "$VERSION_REGISTRY" ]; then
   echo "Publishing package $VERSION_LATEST";
   npm whoami
   npm publish
 else
-  echo "Version is the same as the version available on the registry";
-  echo "Not publishing anything";
+  echo 'Version is the same as the version available on the registry';
+  echo 'Not publishing anything';
 fi
