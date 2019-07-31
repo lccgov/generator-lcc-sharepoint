f7d2e8abab659c05782d558224008c682815c517
matt <matt.williams@rbcom.onmicrosoft.com>
2019-07-31 16:46:31 +0100

    fixed issue with gulp rename

--- generators/app/index.js
+++ generators/app/index.js
@@ -9,7 +9,7 @@ module.exports = yeoman.Base.extend({
   prompting: function () {
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the ' + chalk.blue('LCC Sharepoint') + ' generator!'
+      'Welcome to the ' + chalk.blue('LCC Sharepoint') + ' generator ' + chalk.green(parentConfig.version)
     ));
 
     var prompts = [
